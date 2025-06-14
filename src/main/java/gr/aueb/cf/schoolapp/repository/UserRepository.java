c120abd97b090778813fff1651244dd7e155529b
Katerina Savidaki <ksavvidaki@gmail.com>
2025-06-14 15:54:18 +0300

    fix: replaced vat with afm

--- src/main/java/gr/aueb/cf/schoolapp/repository/UserRepository.java
+++ src/main/java/gr/aueb/cf/schoolapp/repository/UserRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
     Optional<User> findByUsername(String username);
-    Optional<User> findByVat(String vat);
+    Optional<User> findByAfm(String afm);
 }
